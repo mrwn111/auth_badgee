911c1c61910f26b6e6eef925442dc5f20e1bac46
M-barg <mberger@guardiaschool.fr>
2025-03-26 12:24:34 +0100

    Adds WiFi connectivity
    Configures the device to connect to a WiFi network.
    This allows for future functionalities that require internet access.

--- auth_badge/src/main.cpp
+++ auth_badge/src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <WiFi.h>
 
 #define RST_PIN 5
 #define SS_PIN 6
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
+const char *ssid = "WifiCadeau";
+const char *motDePasseWifi = "CadeauWifi";
+
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
+  Serial.println("Connexion au réseau WiFi en cours");
+  WiFi.begin(ssid, motDePasseWifi);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.write('.');
+  }
+  Serial.println("\nWiFi connecté");
   SPI.begin(); 
 
   mfrc522.PCD_Init(); 
