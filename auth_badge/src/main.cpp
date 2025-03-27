7f03e00cdb1a621da50ca7fdfba291c6237cbbf6
alexi <blazemk0122@gmail.com>
2025-03-27 12:00:00 +0100

    Animation entre badge fini

--- auth_badge/src/main.cpp
+++ auth_badge/src/main.cpp
@@ -8,8 +8,6 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-//#define HOST_NAME "http://10.1.42.201:8000/"
-//#define HOST_PORT 80
 
 #define RST_PIN 5
 #define SS_PIN 21
@@ -26,25 +24,9 @@ unsigned long previousMillis = 0; // Store the last time the text was displayed
 const long interval = 5000;       // 5 seconds interval (5000 milliseconds)
 bool badgeDisplayed = false;      // Flag to track if the badge text was displayed
 
-// Animation data
-const unsigned char epd_bitmap_ezgif [] PROGMEM = {
-    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
-    0xff, 0x3f, 0xff, 0xff, 0xf8, 0x11, 0xff, 0xff, 0xf0, 0x00, 0x47, 0xff, 0xc0, 0x00, 0x00, 0xff, 
-    0x80, 0x00, 0x00, 0x3f, 0x80, 0x00, 0x00, 0x0f, 0x80, 0x00, 0x00, 0x07, 0x80, 0x00, 0x00, 0x03, 
-    0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 
-    0xc0, 0x00, 0x00, 0x00, 0xe0, 0x00, 0x00, 0x00, 0xf0, 0x00, 0x00, 0x00, 0xf8, 0x00, 0x00, 0x00, 
-    0xfc, 0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x00, 
-    0xfc, 0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x01, 0xfc, 0x00, 0x00, 0x03, 
-    0xfc, 0x00, 0x00, 0x0f, 0xfe, 0x00, 0x00, 0x1f, 0xff, 0x00, 0x00, 0xff, 0xff, 0xc0, 0x1f, 0xff
-};
+unsigned long loadingMillis = 0;  // Store the last time the "En attente" message was updated
+int loadingState = 0;             // Used to cycle through "En attente", "En attente.", "En attente..", "En attente..."
 
-// Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 144)
-const int epd_bitmap_allArray_LEN = 1;
-const unsigned char* epd_bitmap_allArray[1] = {
-    epd_bitmap_ezgif
-};
-
-// Setup the display and WiFi
 void setup()
 {
   delay(1000);
@@ -55,7 +37,7 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.write('.');
+    Serial.write('.');  // Show dots while waiting for connection
   }
   Serial.println("\nWiFi connecté");
 
@@ -82,83 +64,111 @@ void setup()
   display.setCursor(cursorX, 20);
   display.println(message);
   display.display();
-
-
 }
 
 void loop()
 {
   WiFiClient client;
-  //client.connect(HOST_NAME, HOST_PORT);
 
   unsigned long currentMillis = millis(); // Get current time
 
   if (mfrc522.PICC_IsNewCardPresent())
   {
-    Serial.println("Test 1 passé");
     if (mfrc522.PICC_ReadCardSerial())
     {
-      IPAddress myIP = WiFi.localIP();
-      Serial.println(myIP);
-      Serial.println("Test 2 passé");
-      Serial.println("Nouveau Tag RFID :");
-      Serial.print("RFID Tag (Decimal):");
-
       // Clear the display initially
       display.clearDisplay();
 
-      // Display "Le badge est :" for 5 seconds
-      display.setCursor(0, 0); // Position it near the top
+      // Display "Le badge est :"
+      display.setCursor(0, 0); 
       display.setTextSize(1);
       display.println("Le badge est :");
       display.display();
 
-      // Set the previousMillis to currentMillis when the text is displayed
-      previousMillis = currentMillis;
-      badgeDisplayed = true; // Set flag that the badge text is displayed
+      // Display the UID numbers and print to serial in decimal format
+      int cursorX = 10; 
+      int cursorY = 30; 
 
-      // Display the UID numbers
-      int cursorX = 10; // Starting X position for UID
-      int cursorY = 30; // Starting Y position below the "Le badge est :" text
-
+      Serial.print("UID du badge : ");
       for (byte i = 0; i < mfrc522.uid.size; i++)
       {
-        Serial.print(mfrc522.uid.uidByte[i], DEC);
-        Serial.print(" ");
-
-        // Convert the integer to a String
         String message = String(mfrc522.uid.uidByte[i], DEC);
 
-        // Add space between digits by adjusting cursor
         display.setCursor(cursorX, cursorY);
         display.println(message);
 
-        // Increase the X position for the next number
-        cursorX += 25; // Increase space between numbers
-      }
+        cursorX += 25;
 
-      display.display(); // Update the display with the UID
+        // Serial print each byte in decimal
+        Serial.print(mfrc522.uid.uidByte[i], DEC);
+        Serial.print(" ");
+      }
+      Serial.println();  // Newline after UID print to serial
+
+      display.display();
+      
+      // Set the flag to indicate the badge has been displayed
+      badgeDisplayed = true;
+      previousMillis = currentMillis; // Store the current time to start the 5-second countdown
     }
   }
 
-  // If 5 seconds have passed, clear the display and reset flag
+  // If 5 seconds have passed since displaying the badge, clear the display
   if (badgeDisplayed && (currentMillis - previousMillis >= interval))
   {
     // Clear the display (clear the badge and UID)
     display.clearDisplay();
     display.display();
 
-    // Display animation after UID disappears
-    for (int i = 0; i < epd_bitmap_allArray_LEN; i++)
-    {
-      // Draw the bitmap from PROGMEM (bitmap in array)
-      display.clearDisplay();
-      display.drawBitmap(0, 0, epd_bitmap_allArray[i], SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
-      display.display();
-      delay(200); // Delay between frames to control animation speed
-    }
+    // Reset flag so the display doesn't keep clearing
+    badgeDisplayed = false;
+  }
 
-    badgeDisplayed = false; // Reset the flag to stop further clearing
+  // If no badge text is displayed, show "Loading..." or "En attente"
+  if (!badgeDisplayed)
+  {
+    // Update the "En attente" message every 500 milliseconds
+    if (currentMillis - loadingMillis >= 500)
+    {
+      loadingMillis = currentMillis;  // Update the last time the message was updated
+
+      // Clear the display and show the "En attente" message
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setCursor(30, SCREEN_HEIGHT / 2);  // Center the "En attente" text vertically
+
+      // Cycle through "En attente", "En attente.", "En attente..", "En attente..."
+      if (loadingState == 0)
+      {
+        display.println("En attente");
+      }
+      else if (loadingState == 1)
+      {
+        display.println("En attente.");
+      }
+      else if (loadingState == 2)
+      {
+        display.println("En attente..");
+      }
+      else if (loadingState == 3)
+      {
+        display.println("En attente..:)");
+      }
+      else if (loadingState == 4)
+      {
+        display.println("En attente..");
+      }
+      else if (loadingState == 5)
+      {
+        display.println("En attente.");
+      }
+
+
+      display.display();
+
+      // Update the loading state for the next cycle
+      loadingState = (loadingState + 1) % 6;  // Cycle between 0, 1, 2, 3
+    }
   }
 
   delay(100); // Reduce the delay for smoother operation
